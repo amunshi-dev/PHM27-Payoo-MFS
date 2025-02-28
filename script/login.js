c3ef827239585d79db9e38af5780994d57a1be31
amunshispck <amunshi.apps@gmail.com>
2025-02-28 10:01:14 +0400

    Finish up login.js

--- script/login.js
+++ script/login.js
@@ -1,13 +1,41 @@
 const loginBtn = document.getElementById("login-btn");
 
 loginBtn.addEventListener("click", (e) => {
-  e.preventDefault();
-  const accNum = parseInt(
-    document.querySelectorAll(".login-form input")[0].value
-  );
-  const accPin = parseInt(
-    document.querySelectorAll(".login-form input")[1].value
-  );
-  window.location.href = "./home.html";
-  console.log(accNum, accPin);
+ e.preventDefault();
+ const accNum = parseInt(
+  document.querySelectorAll(".login-form input")[0].value
+ );
+ const accPin = parseInt(
+  document.querySelectorAll(".login-form input")[1].value
+ );
+ const validatedAcc = validateAccNum(accNum)
+ const validatedPin = validatePinNum(accPin)
+ if (!validatedAcc || !validatedPin) {
+  if (!validatedAcc) {
+   console.log('Account number not valid.')
+  } else {
+   console.log('Pin number not valid.')
+  }
+ } else {
+  window.location.href = './home.html'
+ }
 });
+
+
+function validateAccNum(n) {
+ n = String(n)
+ const isFullDigit = n.length == 10;
+ if (isFullDigit) {
+  return true
+ } else {
+  return false
+ }
+}
+
+function validatePinNum(p) {
+ if (p == 1234) {
+  return true
+ } else {
+  return false
+ }
+}
